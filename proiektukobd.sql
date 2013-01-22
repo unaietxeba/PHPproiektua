0d853dd0404cd4330e471efef9c20185d5d6a03b
ikasle <ikasle@UNAIEtxeba>
2013-01-22 14:46:34 +0100

    priektuaren hasiera

---
+++ proiektukobd.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 22-01-2013 a las 14:44:55
+-- Versión del servidor: 5.5.25a
+-- Versión de PHP: 5.4.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `proiektukobd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `cif_cliente` int(11) NOT NULL,
+  `nombre` varchar(20) COLLATE utf8_bin NOT NULL,
+  `apellido` varchar(20) COLLATE utf8_bin NOT NULL,
+  `telefono` int(11) NOT NULL,
+  PRIMARY KEY (`cif_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `coche`
+--
+
+CREATE TABLE IF NOT EXISTS `coche` (
+  `cod_coche` int(11) NOT NULL AUTO_INCREMENT,
+  `marca` varchar(20) COLLATE utf8_bin NOT NULL,
+  `modelo` varchar(20) COLLATE utf8_bin NOT NULL,
+  `caracteristicas` varchar(100) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`cod_coche`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE IF NOT EXISTS `venta` (
+  `num_venta` int(11) NOT NULL,
+  `cod_coche` int(11) NOT NULL,
+  `cif_cliente` int(11) NOT NULL,
+  PRIMARY KEY (`num_venta`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
