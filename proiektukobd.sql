4894b554863a19d9c15263abab44283d3004fdb5
ikasle <ikasle@UNAIEtxeba>
2013-02-08 11:18:05 +0100

    priektuaren hasiera

--- proiektukobd.sql
+++ proiektukobd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 08-02-2013 a las 10:40:14
+-- Tiempo de generación: 08-02-2013 a las 11:17:36
 -- Versión del servidor: 5.5.25a
 -- Versión de PHP: 5.4.4
 
@@ -27,12 +27,62 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `cliente` (
-  `cif_cliente` varchar(255) COLLATE utf8_bin NOT NULL,
+  `cif_cliente` int(11) NOT NULL,
   `nombre` varchar(15) COLLATE utf8_bin NOT NULL,
   `apellido` varchar(15) COLLATE utf8_bin NOT NULL,
   `telefono` varchar(15) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`cif_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`cif_cliente`, `nombre`, `apellido`, `telefono`) VALUES
+(11111111, 'mikel', 'txurruka', '365487523');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `coche`
+--
+
+CREATE TABLE IF NOT EXISTS `coche` (
+  `cod_coche` int(11) NOT NULL,
+  `marca` varchar(15) COLLATE utf8_bin NOT NULL,
+  `modelo` varchar(15) COLLATE utf8_bin NOT NULL,
+  `caracteristicas` varchar(15) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`cod_coche`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `coche`
+--
+
+INSERT INTO `coche` (`cod_coche`, `marca`, `modelo`, `caracteristicas`) VALUES
+(3333333, 'Renault', 'Clio', 'Sport'),
+(4444444, 'fiat', 'uno', 'turbo'),
+(11111111, 'nissan', 'almera', 'simple');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE IF NOT EXISTS `venta` (
+  `num_venta` int(11) NOT NULL AUTO_INCREMENT,
+  `cod_coche` varchar(15) COLLATE utf8_bin NOT NULL,
+  `cif_cliente` varchar(15) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`num_venta`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`num_venta`, `cod_coche`, `cif_cliente`) VALUES
+(1, '2222222', '11111111');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
